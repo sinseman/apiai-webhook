9b6c35032e3f2fd407c904f9a574a59b04e7a799
thomas <thomas@debian.debian>
2017-09-16 12:54:18 +0200

    pioupiou

--- web/pioupiou.php
+++ web/pioupiou.php
@@ -6,25 +6,33 @@ require __DIR__ . '/../vendor/autoload.php';
 
 
 //$rawData = $request->getContent(); // This is how you would retrieve this with Laravel or Symfony 2.
-$rawData = file_get_contents("php://input");
-$request = new \APIAI\Request\Request($rawData);
-$result = $request->getResult();
-$station_id = $result['parameters']['station_id'];
 
-//var_dump($request);exit;
 
-$curl = new Curl\Curl();
-$curl->get('http://api.pioupiou.fr/v1/live/'.$station_id);
+try {
+		
+	$rawData = file_get_contents("php://input");
+	$request = new \APIAI\Request\Request($rawData);
+	$result = $request->getResult();
+	$station_id = $result['parameters']['station_id'];
 
-if ($curl->error) {
-    //$curl->error_code;
-    $message = 'Erreur de communication avec Pioupiou';
-}
-else {
-    $response = json_decode($curl->response, true);
+	//var_dump($request);exit;
 
-    $measurements = $response['data']["measurements"];
-    $message = $response['data']['meta']['name']." vent moyen ".$measurements['wind_speed_avg'].' kilomètre heure';
+	$curl = new Curl\Curl();
+	$curl->get('http://api.pioupiou.fr/v1/live/'.$station_id);
+
+	if ($curl->error) {
+	    //$curl->error_code;
+	    $message = 'Erreur de communication avec Pioupiou';
+	}
+	else {
+	    $response = json_decode($curl->response, true);
+
+	    $measurements = $response['data']["measurements"];
+	    $message = $response['data']['meta']['name']." vent moyen ".$measurements['wind_speed_avg'].' kilomètre heure';
+	}
+
+} catch (Exception $e) {
+	$message = $e->getMessage();
 }
 
 $response = new \APIAI\Response\Response('pioupiou');
