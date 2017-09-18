d852dc292940dedba0add0168b8fafb29dad74ea
thomas <thomas@debian.debian>
2017-09-18 22:17:13 +0200

    add wind directions

--- web/pioupiou.php
+++ web/pioupiou.php
@@ -22,7 +22,55 @@ try {
 	    $response = json_decode($curl->response, true);
 
 	    $measurements = $response['data']["measurements"];
-	    $message = $response['data']['meta']['name']." vent moyen ".$measurements['wind_speed_avg'].' kilomètre heure';
+	    $wind_heading = $measurements['wind_heading'];
+
+		$degrees = array(
+			348,75,
+			11,25,
+			33,75,
+			56,25,
+			78,75,
+			101,25,
+			123,75,
+			146,25,
+			168,75,
+			191,25,
+			213,75,
+			236,25,
+			258,75,
+			281,25,
+			303,75,
+			326,25,
+		);
+
+		$directions = array(
+			'Nord',
+			'Nord-Nord-Est',
+			'Nord-Est',
+			'Est-Nord-Est',
+			'Est',
+			'Est-Sud-Est',
+			'Sud-Est',
+			'Sud-Sud-Est',
+			'Sud',
+			'Sud-Sud-Ouest',
+			'Sud-Ouest',
+			'Ouest-Sud-Ouest',
+			'Ouest',
+			'Ouest-Nord-Ouest',
+			'Nord-Ouest',
+			'Nord-Nord-Ouest',
+		);
+
+		foreach ($degrees as $k => $degree) {
+			$nextDegree = (isset($degree[$k + 1]))? $degree[$k + 1]: $degree[0];
+
+			if ($wind_heading >= $degree && $wind_heading < $nextDegree){
+				break;
+			}
+		}
+
+	    $message = $response['data']['meta']['name']." vent moyen ".$directions[$k].' '.$measurements['wind_speed_avg'].' kilomètre heure';
 	}
 
 } catch (TypeError $e) {
